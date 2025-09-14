fc907c5ef82087ab0d38798da7151eb7314d59b7
Peter kanyori <peterynaenah23@gmail.com>
2025-09-14 12:37:50 +0300

    scoping

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function overwriteBestCustomer(newCustomer){
     bestCustomer = newCustomer;
 }
     function changeLeastFavoriteCustomer(){
-        LeastFavoriteCustomer = 'Martin'
+        leastFavoriteCustomer = 'Martin'
 
     }
 
