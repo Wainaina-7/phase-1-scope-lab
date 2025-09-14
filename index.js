5ddc5be6d0c5d4234877bbb800465195a46d2e7e
Peter kanyori <peterynaenah23@gmail.com>
2025-09-14 12:30:25 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Mike';
+function customerName(){
+    customerName = 'bob'
+}
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newCustomer){
+    bestCustomer = newCustomer;
+}
+    function changeLeastFavoriteCustomer(){
+        LeastFavoriteCustomer = 'Martin'
+
+    }
+
